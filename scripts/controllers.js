31d220f79a4f57e0f43d7933cad90b6a9e6e1a90
Viktor Varland <varl@vlv.io>
2019-11-14 16:03:26 +0100

    fix: ignore localstore props related to headerbar

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -20,7 +20,17 @@ var cacheCleanerControllers = angular.module('cacheCleanerControllers', [])
         $scope.dbKeys = [];
         $scope.ssKeys = [];
 
-        var reservedStorageKeys = ['key', 'getItem', 'setItem', 'removeItem', 'clear', 'length'];
+        var reservedStorageKeys = [
+            'key',
+            'getItem',
+            'setItem',
+            'removeItem',
+            'clear',
+            'length',
+            'dhis2.menu.ui.headerBar.title',
+            'dhis2.menu.ui.headerBar.link',
+            'dhis2.menu.ui.headerBar.userStyle',
+        ];
 
         for(var key in $window.sessionStorage){
             if (reservedStorageKeys.indexOf(key) === -1) {
