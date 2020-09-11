609df9fa70634b992a3743fafa2271c94cbde959
Jan-Gerke Salomon <Mohammer5@users.noreply.github.com>
2020-09-11 14:30:57 +0200

    feat(deleting dbs): succeed even if db does not exist (DHIS2-9492) (#282)

--- src/indexedDb/clearDatabasesByKey.js
+++ src/indexedDb/clearDatabasesByKey.js
@@ -1,3 +1,6 @@
 import { deleteDb } from './deleteDb'
 
-export const clearDatabasesByKey = keys => Promise.all(keys.map(deleteDb))
+export const clearDatabasesByKey = keys => {
+    const allDeleteProcesses = keys.map(key => deleteDb(key, true))
+    return Promise.all(allDeleteProcesses)
+}
